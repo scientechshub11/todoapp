2e25f140b007a1e7e1cd59afdc066ec809ab84bd
scientechshub11 <56425994+scientechshub11@users.noreply.github.com>
2026-02-24 02:41:32 +0530

    commited changes

--- src/app.js
+++ src/app.js
@@ -3,6 +3,8 @@ const app = express();
 const todoRoutes = require('./routes/todo.routes');
 require('dotenv').config();
 const { sequelize } = require("./config/config");
+const {SQSClient, SendMessageCommand, ReceiveMessageCommand} = require('@aws-sdk/client-sqs')
+const sqs = new SQSClient({region:"ap-south-2"})
 let port = process.env.port||7072;
 app.use(express.json());
 app.use(express.urlencoded({extended: true}));
@@ -22,6 +24,27 @@ app.get('/testapi', async(req,res)=>{
         message:"success"
     })
 })
+
+async function pollMessage() {
+    let QUEUE_URL = 'https://sqs.ap-south-2.amazonaws.com/395512255733/image-processing-queue';
+    let data = await sqs.send(new ReceiveMessageCommand({
+            QueueUrl: QUEUE_URL,
+            MaxNumberOfMessages: 1,
+            WaitTimeSeconds: 10
+
+    }))
+    if (data.Messages) {
+    for (const msg of data.Messages) {
+      console.log("Processing:", msg.Body);
+
+      await sqs.send(new DeleteMessageCommand({
+        QueueUrl: QUEUE_URL,
+        ReceiptHandle: msg.ReceiptHandle
+      }));
+    }
+  }
+}
+pollMessage()
 app.use('/api', todoRoutes)
 // 👇 IMPORTANT: bind to 0.0.0.0
 app.listen(port, '0.0.0.0', () => {
